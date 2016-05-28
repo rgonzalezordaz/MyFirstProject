532e7dec938a18ace9efc8666fa229803a35e3c6
Ricardo Gonzalez <Ricardo@macbook-pro-de-ricardo.local>
2016-05-27 20:34:41 -0500

    final falta reacomodo.

--- My First Project/Start.h
+++ My First Project/Start.h
@@ -9,6 +9,14 @@
 #import <UIKit/UIKit.h>
 
 @interface Start : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *lblMessage;
+@property (weak, nonatomic) IBOutlet UIButton *btn1;
+@property (weak, nonatomic) IBOutlet UIButton *btn2;
+@property (weak, nonatomic) IBOutlet UIImageView *imgView;
+
+
+- (IBAction)btn1Pressed:(id)sender;
+- (IBAction)btn2Pressed:(id)sender;
 
 
 @end
--- My First Project/Start.m
+++ My First Project/Start.m
@@ -17,11 +17,22 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.lblMessage.text = @"Se la creyeron";
 }
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)btn1Pressed:(id)sender {
+    self.lblMessage.text = @ "Btn1Pressed";
+    self.imgView.image = [UIImage imageNamed:@"homer-doh.png"];
+}
+
+- (IBAction)btn2Pressed:(id)sender {
+    self.lblMessage.text = @"Btn2Pressed";
+    self.imgView.image = [UIImage imageNamed:@"homero_simpson.jpg"];
+}
 @end
